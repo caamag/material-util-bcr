c2cc26e64f8e5bee731fed8faa126f058f65766e
Caio Lopes <caiolopesfv@gmail.com>
2024-06-11 09:57:09 -0300

    add

--- newCsat/index.js
+++ newCsat/index.js
@@ -21,8 +21,10 @@ async function getCsatFields() {
         }
 
         let content = `
-            <h3 class="question-title">${field.title_in_portal}</h3>
-            <div class="icons-container"></div>
+            <div class="question-container question${orderFields.length - index}">
+                <h3 class="question-title">${titleWithouTXT}</h3>
+                <div class="icons-container"></div>
+            </div>
         `
         const formContainer = document.querySelector('.csat-form')
         formContainer.insertAdjacentHTML('afterbegin', content)
@@ -33,28 +35,94 @@ async function getCsatFields() {
             let match = regexIconsLength.exec(field.title_in_portal);
             let iconsLength = match ? parseInt(match[1], 10) : 5;
 
-            if (iconsLength) {
-                iconsLength = iconsLength;
-            } else {
-                iconsLength = 5;
-            }
-
-            const iconsContainer = document.querySelectorAll('.icons-container')[index];
             for (let i = 0; i < iconsLength; i++) {
                 const icon = document.createElement('img');
                 icon.className = `icon icon${i}`;
-                iconsContainer.appendChild(icon);
+                icon.src = 'https://con-bcrcx-caio.zendesk.com/hc/theming_assets/01HZSRF1J6ECTZNDGM54G5627V'
+                icon.addEventListener('click', () => { handleClick(icon, i, index) })
+                iconsContainer.appendChild(icon)
             }
-        });
+        } else {
+            const newTextarea = document.createElement('textarea')
+            newTextarea.classList.add('free-comment')
+            iconsContainer.appendChild(newTextarea)
+        }
+    });
 
+    function handleClick(icon, indexIcon, indexQuestion) {
+        const allIcons = icon.parentNode.querySelectorAll('.icon')
+        for (let i = 0; i < allIcons.length; i++) {
+            if (i < indexIcon + 1) {
+                allIcons[i].classList.add('selected')
+                allIcons[i].src = 'https://con-bcrcx-caio.zendesk.com/hc/theming_assets/01HZSTYTC3Q59YES4XWCPX6Q11'
+            } else {
+                allIcons[i].classList.remove('selected')
+                allIcons[i].src = 'https://con-bcrcx-caio.zendesk.com/hc/theming_assets/01HZSRF1J6ECTZNDGM54G5627V'
+            }
+        }
+
+        //updating notes
+        notes[indexQuestion] = indexIcon + 1
+    }
 }
-
 getCsatFields()
 
-function handleclick() {
+function lastString(string) { return string.charAt(string.length - 1) }
 
+function handleSubmit() {
+    const textarea = document.querySelector('.free-comment')
+    const iconsContainerWithTextarea = textarea.parentNode
+    const indexTextarea = iconsContainerWithTextarea.parentNode.classList[1]
+    const index = parseInt(lastString(indexTextarea))
 
-
+    const notesToSubmit = notes.reverse()
+    notesToSubmit[notes.length - 1] = textarea.value;
+    console.log(notesToSubmit);
 }
 
+const submitBnt = document.querySelector('.csat-btn')
+submitBnt.addEventListener('click', () => { handleSubmit() })
 
+
+//atualização do ticket
+async function criarTicketRegister() {
+    const urlZendesk = 'https://bcrcxproxyapi.azurewebsites.net/proxy/lacoste-argentina.zendesk.com/api/v2/requests.json';
+    const customFields = [
+        { id: 27333159915411, value: document.getElementById('open-cadastro-button').value },
+        { id: 27345085222419, value: document.getElementById('registration').value },
+    ];
+
+    const payloadTicket = {
+        request: {
+            subject: "Dúvida sobre: cadastro",
+            requester: {
+                email: document.getElementById('email-cadastro').value,
+                name: document.getElementById('name-cadastro').value
+            },
+            comment: {
+                body: document.getElementById('description-cadastro').value,
+            },
+            custom_fields: customFields
+        }
+    };
+
+    try {
+        const response = await fetch(urlZendesk, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(payloadTicket)
+        });
+
+        if (!response.ok) {
+            throw new Error('Erro ao criar o ticket');
+        }
+        closeAllModals();
+        showToast('Ticket criado com sucesso!');
+
+    } catch (error) {
+        console.error('Erro:', error);
+        alert('Erro ao criar o ticket');
+    }
+}
