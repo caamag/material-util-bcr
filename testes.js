d3592d2ac2ded5725f6e2cd7ad9e05b6f6abc08b
BCR-0632 <caiolopesfv@gmail.com>
2024-06-20 16:28:53 -0300

    Enviando atualizações

--- testes.js
+++ testes.js
@@ -1,33 +1,33 @@
-document.querySelectorAll(".website-links li").forEach((item) => {
-  const headerSections = document.querySelectorAll(".hover-content");
 
-  item.addEventListener("mouseover", () => {
-    headerSections.forEach((section) => {
-      if (section.classList.contains(item.classList[0])) {
-        section.classList.add("show-hover-content");
-      } else {
-        section.classList.remove("show-hover-content");
-      }
-    });
-  });
+const customFields = [
+    { id: 28362344838035, value: document.querySelector('#purchase-registro').value }
+];
 
-  item.addEventListener("mouseout", () => {
-    headerSections.forEach((section) => {
-      section.classList.remove("show-hover-content");
-    });
-  });
+const payloadTicket = {
+    request: {
+        subject: "Hablar sobre el orden",
+        requester: {
+            email: document.getElementById('email-purchase').value,
+            name: document.getElementById('name-purchase').value
+        },
+        comment: {
+            body: document.getElementById('description').value,
+        },
+        custom_fields: customFields,
+          tags: [
+          "form_guide_argentina"
+        ]
+    }
+};
 
-  headerSections.forEach((section) => {
-    section.addEventListener("mouseover", () => {
-      section.classList.add("show-hover-content");
-    });
-    section.addEventListener("mouseout", () => {
-      section.classList.remove("show-hover-content");
-    });
-  });
-});
 
-if (window.location.href !== "https://obrtecnologia.zendesk.com/hc/pt-br") {
-  const websiteLinks = document.querySelector(".website-links ul");
-  websiteLinks.style.display = "none";
+
+if (window.location.href.startsWith('https://lacostebrazil.zendesk.com/hc/pt-br/sections/')) {
+    const duplicateChild = document.querySelector('.blocks-list li:last-child');
+    duplicateChild.style.display = 'none'
+}
+
+if (window.location.href === 'https://lacoste-argentina.zendesk.com/hc/pt-br/requests/new?ticket_form_id=28362721571859') {
+    const fieldEmail = document.querySelector('.request_anonymous_requester_email label')
+    fieldEmail.innerText = 'Correo Electronico'
 }
