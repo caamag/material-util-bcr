6180656890cfe4ed5bc03ced8e4474d10fb1d011
Caio Lopes <caiolopesfv@gmail.com>
2024-06-27 16:36:56 -0300

    teste

--- testes.js
+++ testes.js
@@ -42,3 +42,33 @@ async function createRequests() {
 }
 
 createRequests()
+
+
+//ajustando formulário de negativa de ticket
+if (window.location.href === 'https://lacoste-argentina.zendesk.com/hc/pt-br/requests/new?ticket_form_id=30425996426003') {
+    const subject = document.querySelector('.request_subject')
+    subject.style.display = 'none'
+
+    const description = document.querySelector('.request_description');
+    description.style.display = 'none'
+
+    const ticketIDField = document.querySelector('.request_custom_fields_29035220664979')
+    ticketIDField.style.display = 'none'
+
+    const title = document.querySelector('h1')
+    title.style.textAlign = 'center'
+    title.innerText = 'Enviar Solicitación'
+
+    const hero = document.querySelector('.hero')
+    hero.style.display = 'none'
+
+    const selectedForm = document.querySelector('.request_ticket_form_id')
+    selectedForm.style.display = 'none'
+
+    const fields = document.querySelectorAll('.form-field')
+    fields.forEach(field => {
+        if (field.querySelector('.upload-dropzone')) {
+            field.style.display = 'none'
+        }
+    })
+}
