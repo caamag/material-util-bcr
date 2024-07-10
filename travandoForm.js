03070085c613a47749cfee18a7c248d5196fdd63
BCR-0632 <caiolopesfv@gmail.com>
2024-07-10 12:55:22 -0300

    Enviando arquivo de bloqueio de formulário com informações faltantes

---
+++ travandoForm.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('form[data-form][data-form-type="request"]');
+
+    if (form) {
+        const submitBtns = form.querySelectorAll('[type="submit"]')
+        submitBtns.forEach(button => {
+            button.addEventListener('click', (e) => {
+                e.preventDefault()
+                
+                const npsField = document.querySelector('#request_custom_fields_30588810743571');
+                if (npsField.value === '') {
+                    alert('Nos dê uma nota de 0 a 10 para que o formulário seja enviado!')
+                    e.preventDefault()
+                    return;
+                } 
+
+                form.submit();
+            })  
+        })
+    }
+})
