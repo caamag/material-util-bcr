395eeb70c024ee23444aeba887dbb3789365566a
BCR-0632 <caiolopesfv@gmail.com>
2024-08-05 12:49:42 -0300

    Enviando atualizações

--- travandoForm.js
+++ travandoForm.js
@@ -1,39 +1,31 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.querySelector('form[data-form][data-form-type="request"]');
-    const firstForm = document.querySelector('#request_custom_fields_30897134051603');
-    const popUp = document.querySelector(".csat-popup");
+const submitBtnOppo = document.querySelector(".oppo-submit-btn")
+submitBtnOppo.addEventListener("click", (e) => {
+    const popUp = document.querySelector(".csat-popup")
+    e.preventDefault()
 
-    if (form) {
-        const submitBtns = form.querySelectorAll('[type="submit"]')
-        submitBtns.forEach(button => {
-            button.addEventListener('click', (e) => {
-                e.preventDefault()
-
-                const npsField = document.querySelector('#request_custom_fields_30588810743571');
-                if (npsField.value === '') {
-                    e.preventDefault()
-                    popUp.querySelector('p').innerHTML = 'Dê uma nota de 0 a 10.'
-                    popUp.style.visibility = 'visible'
-                    setTimeout(() => {
-                        popUp.style.visibility = 'hidden'
-                    }, 4000)
-                    return
-                } 
-
-                if (firstForm.value === '') {
-                    e.preventDefault()
-                    popUp.style.visibility = 'visible'
-                    popUp.querySelector('p').innerHTML = 'Nos diga se o problema foi ou não resolvido.'
-                    setTimeout(() => {
-                        popUp.style.visibility = 'hidden'
-                    }, 4000)
-                    return
-                }
-
-                setTimeout(() => {
-                    window.location.replace('https://www.oppo.com/br/')
-                }, 200)
-            })  
-        })
+    if (solvedInput.value === '') {
+        e.preventDefault()
+        popUp.style.visibility = 'visible'
+        popUp.querySelector("p").innerHTML = 'Nos diga se o problema foi ou não resolvido.'
+        setTimeout(() => {
+            popUp.style.visibility = 'hidden'
+        }, 4000)
     }
+
+    const questions = document.querySelectorAll(".iRSmTf");
+    questions.forEach(question => {
+        const title = question.querySelector("label").innerText
+        if (title.includes('Na escala de 1 a 10')) {
+            const npsInput = question.querySelector("input")
+            if (npsInput.value === '') {
+                e.preventDefault()
+                popUp.style.visibility = 'visible'
+                popUp.querySelector("p").innerHTML = 'Nos dê uma nota de 0 a 10 para o atendimento.'
+                setTimeout(() => {
+                    popUp.style.visibility = 'hidden'
+                }, 4000)
+            }
+        }
+    })
+    
 })
