076626b9ebeec43173c8649134605010292e6ede
BCR-0632 <caiolopesfv@gmail.com>
2024-08-05 10:03:31 -0300

    justando trava do form

--- travandoForm.js
+++ travandoForm.js
@@ -1,25 +1,39 @@
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.querySelector('form[data-form][data-form-type="request"]');
+    const firstForm = document.querySelector('#request_custom_fields_30897134051603');
+    const popUp = document.querySelector(".csat-popup");
 
     if (form) {
         const submitBtns = form.querySelectorAll('[type="submit"]')
         submitBtns.forEach(button => {
             button.addEventListener('click', (e) => {
                 e.preventDefault()
-                
+
                 const npsField = document.querySelector('#request_custom_fields_30588810743571');
                 if (npsField.value === '') {
-                    alert('Nos dê uma nota de 0 a 10 para que o formulário seja enviado!')
                     e.preventDefault()
-                    return;
+                    popUp.querySelector('p').innerHTML = 'Dê uma nota de 0 a 10.'
+                    popUp.style.visibility = 'visible'
+                    setTimeout(() => {
+                        popUp.style.visibility = 'hidden'
+                    }, 4000)
+                    return
                 } 
 
-                form.submit();
+                if (firstForm.value === '') {
+                    e.preventDefault()
+                    popUp.style.visibility = 'visible'
+                    popUp.querySelector('p').innerHTML = 'Nos diga se o problema foi ou não resolvido.'
+                    setTimeout(() => {
+                        popUp.style.visibility = 'hidden'
+                    }, 4000)
+                    return
+                }
+
+                setTimeout(() => {
+                    window.location.replace('https://www.oppo.com/br/')
+                }, 200)
             })  
         })
     }
 })
-
-if (/https:\/\/oppo-do-brasil\.zendesk\.com\/hc\/pt-br\/requests\/\d+/.test(window.location.href)) {
-    window.location.href = 'https://centraldocliente.lg.com.br/hc/pt-br'
-}
