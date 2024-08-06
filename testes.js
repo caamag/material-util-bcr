c7286d95cbb5f0a4b11a39cde1b3e8d1e46a0b6b
Caio Lopes <caiolopesfv@gmail.com>
2024-08-06 10:24:53 -0300

    Enviando atualizações

--- testes.js
+++ testes.js
@@ -1,9 +1,15 @@
-const deleteItem = (url, element) => {
-    const currentUrl = window.location.href
-    if (currentUrl !== url) {
-        element.style.display = 'none'
-    }
+//csat
+async function getCSATForms() {
+    const res = await fetch('/api/v2/ticket_forms');
+    const data = await res.json()
+
+    const regexCSAT = /CSAT/
+    const regexNPS = /NPS/
+
+    const csatForms = data.ticket_forms.filter(form => regexCSAT.test(form.name) || regexNPS.test(form.name));
+    const baseURL = 'ticket_form_id='
+    const urls = csatForms.map(form => `${baseURL}${form.id}`)
+
+    return urls.some(url => window.location.href.includes(url))
 }
 
-const iconHeader = document.querySelector('.logo-header')
-deleteItem('https://solicita.rivierasl.com.br/hc/pt-br', iconHeader)
