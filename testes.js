72e26749cb1f09f44354ca3750e920e8811d61e1
Caio Lopes <caiolopesfv@gmail.com>
2024-08-21 15:33:29 -0300

    Enviando atualizações

--- testes.js
+++ testes.js
@@ -1,14 +1,25 @@
-//csat
-async function getCSATForms() {
-    const res = await fetch('/api/v2/ticket_forms');
-    const data = await res.json()
-
-    const regexCSAT = /CSAT/
-    const regexNPS = /NPS/
-
-    const csatForms = data.ticket_forms.filter(form => regexCSAT.test(form.name) || regexNPS.test(form.name));
-    const baseURL = 'ticket_form_id='
-    const urls = csatForms.map(form => `${baseURL}${form.id}`)
-
-    return urls.some(url => window.location.href.includes(url))
+function redirectFormRequest(role) {
+    const login = document.querySelector('.header-links')
+    login.classList.add(`${role === 'anonymous' ? 'delete' : 'visible'}`)
 }
+
+const userRole = window.HelpCenter.user.role;
+redirectFormRequest(userRole)
+
+
+const orangeBtn = document.querySelectorAll(".orange-btn");
+orangeBtn.forEach(btn => {
+    btn.addEventListener('click', () => {
+        window.location.replace('https://api.whatsapp.com/send?1=pt_BR&phone=551130038397&text=Ol%C3%A1,%20gostaria%20da%202%C2%AA%20via%20do%20meu%20boleto')
+    })
+})
+
+let isClicked = false;
+const menuIcon = document.querySelector('.menu-icon')
+menuIcon.addEventListener('click', () => {
+    isClicked = !isClicked;
+    const menuMobile = document.querySelector('.menu-mobile')
+    menuMobile.style.visibility = `${isClicked ? 'visible' : 'hidden'}`
+    menuMobile.style.opacity = `${isClicked ? '1' : '0'}`
+})
+
