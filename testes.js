9e99fd832e44228b93ccae4c3266fc5ad11c54ec
Caio Lopes <caiolopesfv@gmail.com>
2024-09-02 16:01:46 -0300

    Enviando atualizações

--- testes.js
+++ testes.js
@@ -6,96 +6,6 @@ function redirectFormRequest(role) {
 const userRole = window.HelpCenter.user.role;
 redirectFormRequest(userRole)
 
-
-const orangeBtn = document.querySelectorAll(".orange-btn");
-orangeBtn.forEach(btn => {
-    btn.addEventListener('click', () => {
-        window.location.replace('https://api.whatsapp.com/send?1=pt_BR&phone=551130038397&text=Ol%C3%A1,%20gostaria%20da%202%C2%AA%20via%20do%20meu%20boleto')
-    })
-})
-
-let isClicked = false;
-const menuIcon = document.querySelector('.menu-icon')
-menuIcon.addEventListener('click', () => {
-    isClicked = !isClicked;
-    const menuMobile = document.querySelector('.menu-mobile')
-    menuMobile.style.visibility = `${isClicked ? 'visible' : 'hidden'}`
-    menuMobile.style.opacity = `${isClicked ? '1' : '0'}`
-})
-
-
-yesAnswer.addEventListener('change', () => {
-    notAnswer.style.backgroundColor = 'white'
-    notAnswer.style.border = '2px solid rgb(200,200,200)'
-    notAnswer.style.boxShadow = 'none'
-    solvedInput.value = 'csat_sim'
-    console.log(solvedInput.value)
-})
-
-notAnswer.addEventListener('change', () => {
-    yesAnswer.style.backgroundColor = 'white'
-    yesAnswer.style.border = '2px solid rgb(200,200,200)'
-    yesAnswer.style.boxShadow = 'none'
-    solvedInput.value = 'csat_não'
-    console.log(solvedInput.value)
-})
-
-if (solvedInput.value === '') {
-    console.log(solvedInput.value)
-    e.preventDefault()
-    popUp.style.visibility = 'visible'
-    popUp.querySelector("p").innerHTML = 'Nos diga se o problema foi ou não resolvido.'
-    setTimeout(() => {
-        popUp.style.visibility = 'hidden'
-    }, 4000)
-}
-
-const defaultChecks = document.querySelectorAll('input[type="checkbox"]');
-defaultChecks.forEach(check => {
-    if (check.id !== 'yes-answer' && check.id !== 'no-answer') {
-        check.addEventListener('change', () => {
-            const csatValue = `${yesAnswer.checked ? 'csat_sim' : notAnswer.checked ? 'csat_não' : ''}`;
-            solvedInput.value = csatValue
-        })
-    }
-})
-
-const submitBtnOppo = document.querySelector(".oppo-submit-btn")
-submitBtnOppo.addEventListener("click", (e) => {
-    const popUp = document.querySelector(".csat-popup")
-
-    if (solvedInput.value === '' && yesAnswer.checked) {
-        solvedInput.value = 'csat_sim'
-    } else if (solvedInput.value === '' && notAnswer.checked) {
-        solvedInput.value = 'csat_não'
-    } else if (solvedInput.value === '') {
-        console.log(solvedInput.value)
-        e.preventDefault()
-        popUp.style.visibility = 'visible'
-        popUp.querySelector("p").innerHTML = 'Nos diga se o problema foi ou não resolvido.'
-        setTimeout(() => {
-            popUp.style.visibility = 'hidden'
-        }, 4000)
-    }
-
-    const questions = document.querySelectorAll(".iRSmTf");
-    questions.forEach(question => {
-        const title = question.querySelector("label").innerText
-        if (title.includes('Na escala de 1 a 10')) {
-            const npsInput = question.querySelector("input")
-            if (npsInput.value === '') {
-                e.preventDefault()
-                popUp.style.visibility = 'visible'
-                popUp.querySelector("p").innerHTML = 'Nos dê uma nota de 0 a 10 para o atendimento.'
-                setTimeout(() => {
-                    popUp.style.visibility = 'hidden'
-                }, 4000)
-            }
-        }
-    })
-})
-
-
 const showLogOutBtn = document.querySelector('.dropdown-toggle')
 const dropDownContent = document.querySelector('.profile-content-dropdown')
 
@@ -104,3 +14,105 @@ showLogOutBtn.addEventListener('click', () => {
     isVisible = !isVisible;
     dropDownContent.style.display = isVisible ? 'block' : 'none'
 })
+
+//ajustando página de seleção de formulário:
+const getAllTicketForms = async () => {
+    const res = await fetch('/api/v2/ticket_forms');
+    const data = await res.json();
+
+    const regexCSAT = /CSAT/
+    const regexNPS = /NPS/
+
+    const forms = data.ticket_forms.filter(form => !regexCSAT.test(form.name) &&
+        !regexNPS.test(form.name) && form.end_user_visible === true && form.active === true && form.restricted_brand_ids[0] === 16092059503380
+    )
+    const selectFormContainer = document.querySelector(".select-form-content");
+
+    forms.map(form => {
+        let content = `
+            <a href="https://niky8517.zendesk.com/hc/pt-br/requests/new?ticket_form_id=${form.id}">${form.name}</a>    
+        `;
+
+        selectFormContainer.insertAdjacentHTML('afterbegin', content);
+    })
+
+    let isVisible = false;
+    const showLinksBtn = document.querySelector(".show-form-btn");
+    showLinksBtn.addEventListener("click", () => {
+        const links = document.querySelectorAll('.select-form-content a')
+        links.forEach(link => {
+            link.style.display = isVisible ? 'flex' : 'none'
+        })
+
+        isVisible = !isVisible
+    });
+}
+
+
+const getTicketForms = async () => {
+    const res = await fetch('/api/v2/ticket_forms');
+    const data = await res.json()
+
+    const regexCSAT = /CSAT/
+    const regexNPS = /NPS/
+
+    const forms = data.ticket_forms.filter(form => !regexCSAT.test(form.name) &&
+        !regexNPS.test(form.name) && form.end_user_visible === true &&
+        form.active === true &&
+        form.restricted_brand_ids[0] === 16092059503380
+    )
+
+    const baseURL = 'ticket_form_id=';
+    const urls = forms.map(form => `${baseURL}${form.id}`)
+    return urls.some(url => window.location.href.includes(url))
+
+}
+
+const showLinks = async () => {
+    const res = await fetch('/api/v2/ticket_forms');
+    const data = await res.json()
+
+    const regexCSAT = /CSAT/
+    const regexNPS = /NPS/
+
+    const forms = data.ticket_forms.filter(form => !regexCSAT.test(form.name) &&
+        !regexNPS.test(form.name) && form.end_user_visible === true &&
+        form.active === true &&
+        form.restricted_brand_ids[0] === 16092059503380
+    )
+
+    forms.map(form => {
+        let content = `
+            <a href="https://niky8517.zendesk.com/hc/pt-br/requests/new?ticket_form_id=${form.id}" class="link-form">${form.display_name}</a><br>
+        `
+        const links = document.querySelector('.forms-link')
+        links.insertAdjacentHTML('afterend', content);
+    })
+}
+
+getTicketForms()
+    .then(isCustomerForm => {
+        if (isCustomerForm) {
+            const requestFormID = document.querySelector('.request_ticket_form_id')
+            const firtInput = document.querySelector('.nesty-input').style.display = 'none'
+
+            let content = `
+                <button class="select-form-btn-first">-</button>
+                <div class="forms-link"></div>
+            `
+            requestFormID.insertAdjacentHTML('afterend', content)
+
+            showLinks()
+            let isVisible = false;
+
+            const showLinkdBtn = document.querySelector('.select-form-btn-first')
+            showLinkdBtn.addEventListener('click', async (e) => {
+                e.preventDefault()
+                isVisible = !isVisible;
+                const links = document.querySelectorAll('.link-form');
+                links.forEach(link => {
+                    link.classList.add(isVisible ? 'visible-links' : 'invisible-links')
+                })
+            })
+        }
+    })
