3308c33aa91c5b4b8e259eb31a00415df1b62bcb
Caio Lopes <caiolopesfv@gmail.com>
2024-09-16 16:01:15 -0300

    Enviando atualizações SKA

--- ska.js
+++ ska.js
@@ -23,3 +23,29 @@ const start = () => {
 document.addEventListener('DOMContentLoaded', () => {
     start()
 })
+
+
+let shoCategories = false;
+const showMoreBtn = document.querySelector('.show-more-btn');
+const allCategories = document.querySelectorAll('.categories-container a');
+showMoreBtn.addEventListener('click', () => {
+    shoCategories = !shoCategories;
+
+    allCategories.forEach((categorie, index) => {
+        if (index > 7 && !shoCategories) {
+            categorie.style.visibility = 'hidden'
+        } else {
+            categorie.style.visibility = 'visible'
+        }
+    })
+
+    showMoreBtn.innerHTML = !shoCategories ? 'Exibir todas as categorias' : 'Exibir menos'
+})
+
+allCategories.forEach((categorie, index) => {
+    if (index > 7) {
+        categorie.style.visibility = 'hidden'
+    } else {
+        categorie.style.visibility = 'visible'
+    }
+})
