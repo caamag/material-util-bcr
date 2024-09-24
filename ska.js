3c1668a5fecdcfb88d77e4d11d8b9d69a348940f
BCR-0632 <caiolopesfv@gmail.com>
2024-09-24 10:52:42 -0300

    Enviando atualizações

--- ska.js
+++ ska.js
@@ -24,38 +24,6 @@ document.addEventListener('DOMContentLoaded', () => {
     start()
 })
 
-
-let shoCategories = false;
-const showMoreBtn = document.querySelector('.show-more-btn');
-const allCategories = document.querySelectorAll('.categorie-card');
-showMoreBtn.addEventListener('click', () => {
-    shoCategories = !shoCategories;
-
-    allCategories.forEach((categorie, index) => {
-        if (index > 7 && !shoCategories) {
-            categorie.style.opacity = '0'
-            categorie.style.height = '0px'
-            categorie.style.diplay = 'none'
-        } else {
-            categorie.style.opacity = '1'
-            categorie.style.height = '120px'
-            categorie.style.diplay = 'flex'
-        }
-    })
-
-    showMoreBtn.innerHTML = !shoCategories ? 'Exibir todas as categorias' : 'Exibir menos'
-})
-
-allCategories.forEach((categorie, index) => {
-    if (index > 7) {
-        categorie.style.opacity = '0'
-        categorie.style.height = '0px'
-    } else {
-        categorie.style.opacity = '1'
-        categorie.style.height = '120px'
-    }
-})
-
 //verify if is end user
 const isAdmin = (role) => {
     console.log(role);
